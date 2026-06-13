17063774e142803224fffdb5ee09de700befcc8a
Sorawit <thankzaki@hotmail.com>
2026-06-13 11:23:08 +0700

    Step: 7 Button widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -5,18 +5,64 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset("assets/images/flower.jpg", width: 200, height: 200),
-        SizedBox(height: 20),
-        Image.network(
-          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-          width: 200,
-          height: 200,
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.pinkAccent),
+            onPressed: () {
+              print("กดปุ่มแล้ว TextButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.greenAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว FilledButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              side: BorderSide(color: Colors.blueAccent, width: 2),
+              foregroundColor: Colors.blueAccent,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว OutlinedButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.yellowAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว ElevatedButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
